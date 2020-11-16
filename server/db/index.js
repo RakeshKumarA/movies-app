7019082b7926e4996c1fc04194962e7158dc44f6
RakeshKumarA <rkakumar85@gmail.com>
2020-11-16 14:49:57 +0100

    Added mongo

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://mongo:27017/cinema', { useNewUrlParser: true })
+  .connect(
+    'mongodb+srv://rakesh1234:rakesh1234@cluster0.aajcd.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    { useNewUrlParser: true }
+  )
   .catch((e) => {
     console.error('Connection error', e.message);
   });
