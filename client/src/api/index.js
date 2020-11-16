c6401c108fde8c74216c5ce10bf48d3c6e2b6fbe
RakeshKumarA <rkakumar85@gmail.com>
2020-11-16 15:16:39 +0100

    updatd the proxy

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://server-42q4vyihia-ey.a.run.app/api',
 });
 
 export const insertMovie = (payload) => api.post(`/movie`, payload);
